5c98da2ce1e3024aee2ce38eca2f18e57ba9ac36
Patrick Bucher <patrick.bucher@mailbox.org>
2022-05-15 10:55:49 +0200

    improved logging

--- openbsd_autoinstall.go
+++ openbsd_autoinstall.go
@@ -6,18 +6,25 @@ import (
 	"os"
 )
 
+const (
+	endpoint = "/install.conf"
+	port     = 8000
+)
+
 func main() {
 	if len(os.Args) < 2 {
-		fmt.Fprintf(os.Stderr, "usage: %s [autoinstall file]", os.Args[0])
+		fmt.Fprintf(os.Stderr, "usage: %s [autoinstall file]\n", os.Args[0])
 		os.Exit(1)
 	}
 	autoconfFile := os.Args[1]
 	if _, err := os.Stat(autoconfFile); err != nil {
-		fmt.Fprintf(os.Stderr, "stat file '%s': %v", autoconfFile, err)
+		fmt.Fprintf(os.Stderr, "stat file '%s': %v\n", autoconfFile, err)
 		os.Exit(2)
 	}
-	http.HandleFunc("/install.conf", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc(endpoint, func(w http.ResponseWriter, r *http.Request) {
+		fmt.Printf("serve %s to %s\n", endpoint, r.RemoteAddr)
 		http.ServeFile(w, r, autoconfFile)
 	})
-	http.ListenAndServe("0.0.0.0:8000", nil)
+	fmt.Printf("serving %s on port %d\n", endpoint, port)
+	http.ListenAndServe(fmt.Sprintf("0.0.0.0:%d", port), nil)
 }
